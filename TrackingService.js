95c3cf0b5649b6a097ec5bbc325e7bac4c50dfc0
Dylan Nguyen <d_nguyen@live.com>
2015-02-24 23:18:51 -0500

    Implement twitter integration

--- TrackingService.js
+++ TrackingService.js
@@ -4,7 +4,7 @@ var request = require('request'),
     _ = require('lodash'),
     RSSSource = require('./RSSSource.js'),
     DataStore = require('./DataStore.js');
-
+var TwitterSource = require('./TwitterSource');
 var feedparser = new FeedParser();
 var keywords = ['Google', 'Silicon Valley', 'Microsoft'];
 var sources = [];
@@ -15,16 +15,18 @@ var TrackingService = function() {
 
 TrackingService.prototype.start = function() {
     console.log('Starting Tracking Service');
-    var rssSource = new RSSSource({
-        feed: 'http://feeds.feedburner.com/TechCrunch/',
-        keywords: keywords
-    });
-    rssSource.parse();
-    var vergeSource = new RSSSource({
-        feed: 'http://www.theverge.com/rss/index.xml',
-        keywords: keywords
-    });
-    vergeSource.parse();
+    // var rssSource = new RSSSource({
+    //     feed: 'http://feeds.feedburner.com/TechCrunch/',
+    //     keywords: keywords
+    // });
+    // rssSource.parse();
+    // var vergeSource = new RSSSource({
+    //     feed: 'http://www.theverge.com/rss/index.xml',
+    //     keywords: keywords
+    // });
+    // vergeSource.parse();
+    var twitterSource = new TwitterSource();
+    twitterSource.parse();
 };
 
 module.exports = TrackingService;
--- TwitterSource.js
+++ TwitterSource.js
@@ -1,8 +1,36 @@
 
-var Twit = require('twit');
+var Twit = require('twit'),
+    _ = require('lodash'),
+    config = require('./config.js'),
+    DataStore = require('./DataStore.js');
 
 var TwitterSource = function(options) {
+    this.twit = new Twit({
+        consumer_key: config.twitter.consumerKey,
+        consumer_secret: config.twitter.consumerSecret,
+        access_token: config.twitter.accessToken,
+        access_token_secret: config.twitter.accessTokenSecret
+    });
+};
 
+TwitterSource.prototype.parse = function() {
+    var searchQuery = '"' + config.keywords[0] + '"';
+    _.each(config.keywords, function(word) {
+        searchQuery += ' OR "' + word + '"';
+    });
+    console.log('Searching search/tweets ' + encodeURI(searchQuery));
+    this.twit.get('search/tweets', { q: encodeURI(searchQuery), lang: 'en', count: 25 }, function(err, data, response) {
+        _.each(data.statuses, function(tweet) {
+            console.log(tweet.text);
+            _.each(config.keywords, function(word) {
+                if (tweet.text.indexOf(word) > -1 || tweet.text.indexOf(word.toLowerCase()) > -1) {
+                    console.log(tweet.text);
+                    DataStore.increment(word, 1);
+                    DataStore.addHit(word, { from: tweet.id, title: tweet.text, type: 'tweet' });
+                }
+            });
+        });
+    });
 };
 
 module.exports = TwitterSource;
