70b722f765a0517a5d408028edc04c84dead1492
Dylan Nguyen <d_nguyen@live.com>
2015-02-28 20:28:37 -0500

    Do individual twitter search for each keyword

--- TwitterSource.js
+++ TwitterSource.js
@@ -15,37 +15,33 @@ var TwitterSource = function(options) {
 };
 
 TwitterSource.prototype.parse = function() {
-    var searchQuery = '';
-    for (var i = 0; i < config.keywords.length; i++) {
-        searchQuery += '"' + config.keywords[i] + '"';
-        if (i < config.keywords.length - 1) {
-            searchQuery += ' OR ';
-        }
-    }
-    console.log(searchQuery);
-    console.log('Searching search/tweets ' + encodeURI(searchQuery));
-    this.twit.get('search/tweets', { q: encodeURI(searchQuery), lang: 'en', result_type: 'recent', count: 100 }, function(err, data, response) {
-        if (err) {
-            console.log('error with loading tweets');
-            console.log(err);
-        } else {
-            _.each(data.statuses, function(tweet) {
-                console.log(tweet.text);
-                _.each(config.keywords, function(word) {
-                    if (tweet.text.indexOf(word) > -1 || tweet.text.indexOf(word.toLowerCase()) > -1) {
-                        var created = moment(tweet.created_at, 'dd MMM DD HH:mm:ss ZZ YYYY', 'en');
+    var self = this;
+    _.each(config.keywords, function(word) {
 
-                        DataStore.increment(word, 1);
-                        DataStore.addHit(word, {
-                            from: tweet.id_str,
-                            title: tweet.text,
-                            type: 'tweet',
-                            date: created.toDate()
-                        });
-                    }
+        self.twit.get('search/tweets', { q: encodeURI(word), lang: 'en', result_type: 'recent', count: 100 }, function(err, data, response) {
+            if (err) {
+                console.log('error with loading tweets');
+                console.log(err);
+            } else {
+                _.each(data.statuses, function(tweet) {
+                    console.log(tweet.text);
+                    _.each(config.keywords, function(word) {
+                        if (tweet.text.indexOf(word) > -1 || tweet.text.indexOf(word.toLowerCase()) > -1) {
+                            var created = moment(tweet.created_at, 'dd MMM DD HH:mm:ss ZZ YYYY', 'en');
+
+                            DataStore.increment(word, 1);
+                            DataStore.addHit(word, {
+                                from: tweet.id_str,
+                                title: tweet.text,
+                                type: 'tweet',
+                                date: created.toDate()
+                            });
+                        }
+                    });
                 });
-            });
-        }
+            }
+        });
+
     });
 };
 
